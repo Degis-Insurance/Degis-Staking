27f72276981e37bb0af146e9c92b1083141925c3
liyang-iMac <ylikp@connect.ust.hk>
2021-12-02 23:07:17 +0800

    redeploy add minter

--- scripts/addMinter.js
+++ scripts/addMinter.js
@@ -1,7 +1,9 @@
 const BuyerToken = artifacts.require("BuyerToken");
 const Vault = artifacts.require("PurchaseIncentiveVault");
+const DegisToken = artifacts.require("DegisToken");
 
-const router = "0x040084c7e4a0AA6B4Ac21FaC0AeF1C3928E57Aa9";
+const router = "0xDc5f05ff3188570207f054B9751ba87Cb1488CcD";
+const farmingPool = "0x296dc4362382A4a6C3b057718E505a74d9395EA2";
 
 module.exports = async (callback) => {
   try {
@@ -14,9 +16,13 @@ module.exports = async (callback) => {
     const vault = await Vault.deployed();
     console.log("vault address:", vault.address);
 
+    const degis = await DegisToken.deployed();
+
     // await buyertoken.addMinter(vault.address, { from: account });
 
-    await buyertoken.addMinter(router, { from: account });
+    // await buyertoken.addMinter(router, { from: account });
+
+    await degis.addMinter(farmingPool, { from: account });
 
     callback(true);
   } catch (err) {
