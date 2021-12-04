6f0669c12264258682331988e8a2aea54b9e7fa1
liyang-iMac <ylikp@connect.ust.hk>
2021-12-04 23:21:51 +0800

    add minter for policyflow

--- scripts/addMinter.js
+++ scripts/addMinter.js
@@ -3,16 +3,19 @@ const Vault = artifacts.require("PurchaseIncentiveVault");
 const DegisToken = artifacts.require("DegisToken");
 
 const router = "0xDc5f05ff3188570207f054B9751ba87Cb1488CcD";
-const farmingPool = "0xa74A6E11Ed5554031Aa743e87CA9f02f0ed28528";
+const farmingPool = "0xC3E497a38C62B66620e21F9E7Ca2180d8Cd6716B";
+const policyFlow = "0x19145b0AcFC8F38F59822Be268Ee636A5C055281";
 
 const degis_add = "0x6d3036117de5855e1ecd338838FF9e275009eAc2";
+
+const buyer_add = "0x876431DAE3c10273F7B58567419eb40157CcA9Eb";
 
 module.exports = async (callback) => {
   try {
     const accounts = await web3.eth.getAccounts();
     const account = accounts[0];
 
-    const buyertoken = await BuyerToken.deployed();
+    const buyertoken = await BuyerToken.at(buyer_add);
     console.log("buyer token address:", buyertoken.address);
 
     const vault = await Vault.deployed();
@@ -22,9 +25,9 @@ module.exports = async (callback) => {
 
     // await buyertoken.addMinter(vault.address, { from: account });
 
-    // await buyertoken.addMinter(router, { from: account });
+    await buyertoken.addMinter(policyFlow, { from: account });
 
-    await degis.addMinter(farmingPool, { from: account });
+    // await degis.addMinter(farmingPool, { from: account });
 
     callback(true);
   } catch (err) {
