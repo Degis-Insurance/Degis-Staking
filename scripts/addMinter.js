9920b30c2111f4637757c77aca881b149f8c5f48
liyang-iMac <ylikp@connect.ust.hk>
2021-12-04 21:21:49 +0800

    fdf54

--- scripts/addMinter.js
+++ scripts/addMinter.js
@@ -3,7 +3,7 @@ const Vault = artifacts.require("PurchaseIncentiveVault");
 const DegisToken = artifacts.require("DegisToken");
 
 const router = "0xDc5f05ff3188570207f054B9751ba87Cb1488CcD";
-const farmingPool = "0xFE9D6183c68d15b554232F6719Be297C89216b45";
+const farmingPool = "0xa74A6E11Ed5554031Aa743e87CA9f02f0ed28528";
 
 const degis_add = "0x6d3036117de5855e1ecd338838FF9e275009eAc2";
 
